e2ee9bc41af06065542a29c8bc72fd724b9ac270
Mitos Kalandiel <mitoskalandiel@gmail.com>
2021-09-20 21:35:49 +0100

    a very basic pinmodel and gpio service (as it were)

--- RpiLED.Core/Models/PinModel.cs
+++ RpiLED.Core/Models/PinModel.cs
@@ -1,12 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Device.Gpio;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RpiLED.Core.Services;
+using PinMode = System.Device.Gpio.PinMode;
 
 namespace RpiLED.Core.Models
 {
-    class PinModel
+    internal class PinModel
     {
+        internal GpioService ioService;
+
+        private static int pinLocation;
+
+        public PinValue PinState { get; private set; }
+        public PinMode PinDirection { get; private set; }
+
+        public PinModel(PinScheme scheme, int pinNumber)
+        {
+            ioService = new GpioService();
+            pinLocation = pinNumber;
+            _getPinValue();
+        }
+
+        private void _getPinDirection()
+        {
+            PinDirection = ioService.Gpio.GetPinMode(pinLocation);
+        }
+
+        private void _getPinValue()
+        {
+            PinState = ioService.Gpio.Read(pinLocation);
+        }
+
     }
 }
--- RpiLED.Core/Services/GpioService.cs
+++ RpiLED.Core/Services/GpioService.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Device.Gpio;
 
 namespace RpiLED.Core.Services
 {
+
     internal enum PinScheme
     {
-        Physical,
-        Broadcom,
-        WiringPi
+        Physical = 1,
+        Broadcom = 2,
+        None = -1
     };
 
-    class GpioService
+    internal enum PinDirection
     {
+        In,
+        Out
+    };
+
+    internal enum PinMode
+    {
+        Gpio = 1,
+        Pwm = 2,
+        None = -1
+    };
+
+    internal class GpioService
+    {
+        public GpioController Gpio { get; private set; }
+
+        private void SetScheme()
+        {
+            Gpio = new GpioController(PinNumberingScheme.Board);
+        }
+
+        public GpioService()
+        {
+            SetScheme();
+        }
     }
 }
