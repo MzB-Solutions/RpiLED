e628215d05175552a757aad8824ce715a91b4b44
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2021-09-22 18:04:22 +0100

    added Exception handling from ConsoLovers ConsoleApplication

--- RpiLED.Cli/Bootstrap/AppBootstrap.cs
+++ RpiLED.Cli/Bootstrap/AppBootstrap.cs
@@ -1,4 +1,6 @@
-﻿using ConsoLovers.ConsoleToolkit.Core;
+﻿using System;
+using System.IO;
+using ConsoLovers.ConsoleToolkit.Core;
 using ConsoLovers.ConsoleToolkit.Core.CommandLineArguments;
 using RpiLED.Cli.Properties;
 
@@ -17,6 +19,14 @@ namespace RpiLED.Cli.Bootstrap
                 Console.Write(Resources.ExitPrompt); Console.ReadLine();
             }
         }
+        public override bool HandleException(Exception exception)
+        {
+            if (exception is IOException)
+                Environment.Exit(-1);
 
+            // e.g. do some logging for unknown exceptions...
+            Environment.Exit(666);
+            return true;
+        }
     }
 }
