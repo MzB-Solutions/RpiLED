0fb30b42a07b9346d453fbc09382882c16f79c8a
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2021-09-30 09:29:49 +0100

    added a quick and dirty update.sh - features:   - -h help   - -v be verbose (Default: false)   - -a [Def:0|1] Automatic cleanup [Optional]

---
+++ update.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+# -*- coding: utf-8 -*-
+AUTOCLEAN=0
+fileName=$(basename "$0")
+function printHelp() {
+    printf "%s " "$fileName"
+    printf "%s\n" "Usage help:"
+    echo "##################"
+    printf "%s\n" "Defaults "
+    echo "-a ${AUTOCLEAN} "
+    echo "=================="
+    echo "Parameter help:"
+    printf "%s\n" "-a[utomatic-cleanup] (either 0 or 1) 0 doesn't try to clean the Project/Submodule Directories"
+    printf "%s\n" "-v[erbose] be verbose in output"
+    printf "%s\n" "-h[elp] see this text"
+    exit 0
+}
+function isTrue() {
+    if [[ "${*^^}" =~ ^(TRUE|OUI|Y|O$|ON$|[1-9]) ]]; then return 0;fi
+    return 1
+}
+while getopts ":h:a:v:" opt; do
+    case $opt in
+        h) PRINTHELP=1;;
+        a) AUTOCLEAN="$OPTARG";;
+        v) VERBOSE="-v"
+        \?) echo "Invalid option -$OPTARG" >&2;;
+    esac
+done
+if isTrue "$PRINTHELP"; then
+    printHelp;
+fi
+git fetch $VERBOSE
+echo "git fetch result: $? #####"
+git status $VERBOSE
+echo "git status result: $? #####"
+if [[ "$AUTOCLEAN" -eq 1 ]]; then
+     rm -rf ./vendor/ConsoLovers/
+     rm -rf ./output/
+fi
+git reset --hard $VERBOSE
+echo "git reset result: $? #####"
+git submodule init $VERBOSE
+echo "git submodule init result: $? #####"
+git submodule update $VERBOSE
+echo "git submodule update result: $? #####"
