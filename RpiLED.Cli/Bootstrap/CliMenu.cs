0d123600e0eb469a26f8950a79b906def5ed337f
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2021-10-04 17:04:02 +0100

    introduced EasyConsole and made quick mockup menu
    this should trigger when no args are given into the cli app, after that help should still be displayed
    created a new todo: need to implement appconfig.json for interactive true/false

---
+++ RpiLED.Cli/Bootstrap/CliMenu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using EasyConsole;
+
+namespace RpiLED.Cli.Bootstrap
+{
+    public class CliMenu
+    {
+        public CliMenu()
+        {
+            var menu = new EasyConsole.Menu()
+                .Add("GPIO", () => Output.WriteLine("GPIO selected..", "green"))
+                .Add("PWM", () => Output.WriteLine("PWM selected..", "blue"));
+            menu.Display();
+        }
+    }
+}
--- RpiLED.Cli/Program.cs
+++ RpiLED.Cli/Program.cs
@@ -2,16 +2,25 @@
 using System.Linq;
 using ConsoLovers.ConsoleToolkit.Core;
 using RpiLED.Cli.Bootstrap;
+using EasyConsole;
 
 namespace RpiLED.Cli
 {
     internal class Program
     {
+        /// <summary>
+        /// The main entry point for RpiLED.Cli
+        /// </summary>
+        /// <param name="args">command line parameters passed in from the environment</param>
+        /// TODO(smzb): We should be able to read a config file and define wether interactive mode is enabled or not.
         private static void Main(string[] args)
         {
             Console.WriteLine($@"Number of Arguments: {args.Length}");
             if (args.Length == 0)
             {
+                // Spawn a new cli-menu
+                _ = new CliMenu();
+                // Also set -h for help output
                 args = new[] {"-h"};
             }
             //Console.WriteLine("Hello World!");
