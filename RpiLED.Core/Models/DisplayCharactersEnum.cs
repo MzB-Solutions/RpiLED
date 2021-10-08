c9c8f9f11806d872276f25cabeee1367f230f2cb
Sebastian Meier zu Biesen <smeierzubiesen@gmail.com>
2021-10-08 15:53:21 +0100

    let's create an enum of characters we can display on a 7segment display I have also created the binary values representing the enabled pins for each character these values will get bit shifted into a shift register 74HC595 and on each clock pulse will get output

---
+++ RpiLED.Core/Models/DisplayCharactersEnum.cs
@@ -0,0 +1,22 @@
+﻿namespace RpiLED.Core.Models
+{
+    public enum DisplayCharactersEnum
+    {
+        HexZero = 0b11111100,
+        HexOne = 0b01100000,
+        HexTwo = 0b11011010,
+        HexThree = 0b11110010,
+        HexFour = 0b01100110,
+        HexFive = 0b10110110,
+        HexSix = 0b10111110,
+        HexSeven = 0b11100000,
+        HexEight = 0b11111110,
+        HexNine = 0b11100110,
+        HexA = 0b11101110,
+        HexB = 0b00111110,
+        HexC = 0b10011100,
+        HexD = 0b01111010,
+        HexE = 0b10011110,
+        HexF = 0b10001110
+    }
+}
---
+++ RpiLED.Core/Services/ShiftRegisterService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using RpiLED.Core.Models;
+
+namespace RpiLed.Core.Services
+{
+    public class ShiftRegisterService
+    {
+        public List<DisplayCharactersEnum> Characters =
+        Enum.GetValues(typeof(DisplayCharactersEnum))
+        .Cast<DisplayCharactersEnum>()
+        .ToList();
+    }
+
+
+}
