c4dc5cd4c434c7f158345afa30267ae9f2dfd565
Sebastian Meier zu Biesen <mitoskalandiel@gmail.com>
2021-11-04 16:28:58 +0000

    duh! use the stuff implementeted by system.iot!!! IDIOT!!

--- RpiLED.Core/Services/ShiftRegisterService.cs
+++ RpiLED.Core/Services/ShiftRegisterService.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Data;
 using System.Threading;
+using Iot.Device.Multiplexing;
 using RpiLed.Core;
-using RpiLED.Core.Models;
 
 namespace RpiLED.Core.Services
 {
@@ -12,11 +11,8 @@ namespace RpiLED.Core.Services
 
         public ShiftRegisterService()
         {
-            _sdiPin.PinWrite(false);
-            _rclkPin.PinWrite(false);
-            _srclkPin.PinWrite(false);
-            // Reset pin is active low!!
-            _rstPin.PinWrite(true);
+            var pinMapping = new ShiftRegisterPinMapping(Sdi, Srclk, Rclk);
+            _sr = new ShiftRegister(pinMapping, 8);
         }
 
         #endregion Public Constructors
@@ -25,10 +21,7 @@ namespace RpiLED.Core.Services
 
         ~ShiftRegisterService()
         {
-            _sdiPin.gpioService.Gpio.Dispose();
-            _rclkPin.gpioService.Gpio.Dispose();
-            _srclkPin.gpioService.Gpio.Dispose();
-            _rstPin.gpioService.Gpio.Dispose();
+            _sr.Dispose();
         }
 
         #endregion Private Destructors
@@ -38,17 +31,14 @@ namespace RpiLED.Core.Services
         // Storage Register clock (ST_CP)
         private const int Rclk = 18;
 
-        // Serial Data In  (DS) Pin
-        private const int Sdi = 16;
-
         // Reset (MR) Pin
         private const int Rst = 22;
 
+        // Serial Data In  (DS) Pin
+        private const int Sdi = 16;
+
         // Shift register clock (SH_CP)
         private const int Srclk = 29;
-
-        // Test Led Pin
-        private const int IndLed = 13;
 
         private static readonly byte[] ShiftOutput =
         {
@@ -62,100 +52,55 @@ namespace RpiLED.Core.Services
             0x80
         };
 
-        private readonly PinModel _indPin = new(IndLed, PinService.Gpio);
-        private readonly PinModel _rclkPin = new(Rclk, PinService.Gpio);
-        private readonly PinModel _sdiPin = new(Sdi, PinService.Gpio);
-        private readonly PinModel _rstPin = new(Rst, PinService.Gpio);
-        private readonly PinModel _srclkPin = new(Srclk, PinService.Gpio);
+        private readonly ShiftRegister _sr;
 
         #endregion Private Fields
 
         #region Private Methods
 
-        private void ResetSr()
-        {
-            _rstPin.PinWrite(false);
-            Pulse(_rclkPin);
-            Pulse(_srclkPin);
-            _rstPin.PinWrite(true);
-        }
-
-        private static string ToBinary(int myValue)
-        {
-            var binVal = Convert.ToString(myValue, 2);
-            var bits = 0;
-            var bitblock = 4;
-
-            for (var i = 0; i < binVal.Length; i += bitblock)
-            { bits += bitblock; }
-
-            return binVal.PadLeft(bits, '0');
-        }
-
         private static byte GetBitPattern(char character)
         {
             return character switch
             {
-                '0' => (byte)DisplayCharactersEnum.HexZero,
-                '1' => (byte)DisplayCharactersEnum.HexOne,
-                '2' => (byte)DisplayCharactersEnum.HexTwo,
-                '3' => (byte)DisplayCharactersEnum.HexThree,
-                '4' => (byte)DisplayCharactersEnum.HexFour,
-                '5' => (byte)DisplayCharactersEnum.HexFive,
-                '6' => (byte)DisplayCharactersEnum.HexSix,
-                '7' => (byte)DisplayCharactersEnum.HexSeven,
-                '8' => (byte)DisplayCharactersEnum.HexEight,
-                '9' => (byte)DisplayCharactersEnum.HexNine,
-                'A' => (byte)DisplayCharactersEnum.HexA,
-                'B' => (byte)DisplayCharactersEnum.HexB,
-                'C' => (byte)DisplayCharactersEnum.HexC,
-                'D' => (byte)DisplayCharactersEnum.HexD,
-                'E' => (byte)DisplayCharactersEnum.HexE,
-                'F' => (byte)DisplayCharactersEnum.HexF,
-                'a' => (byte)DisplayCharactersEnum.HexA,
-                'b' => (byte)DisplayCharactersEnum.HexB,
-                'c' => (byte)DisplayCharactersEnum.HexC,
-                'd' => (byte)DisplayCharactersEnum.HexD,
-                'e' => (byte)DisplayCharactersEnum.HexE,
-                'f' => (byte)DisplayCharactersEnum.HexF,
+                '0' => (byte) DisplayCharactersEnum.HexZero,
+                '1' => (byte) DisplayCharactersEnum.HexOne,
+                '2' => (byte) DisplayCharactersEnum.HexTwo,
+                '3' => (byte) DisplayCharactersEnum.HexThree,
+                '4' => (byte) DisplayCharactersEnum.HexFour,
+                '5' => (byte) DisplayCharactersEnum.HexFive,
+                '6' => (byte) DisplayCharactersEnum.HexSix,
+                '7' => (byte) DisplayCharactersEnum.HexSeven,
+                '8' => (byte) DisplayCharactersEnum.HexEight,
+                '9' => (byte) DisplayCharactersEnum.HexNine,
+                'A' => (byte) DisplayCharactersEnum.HexA,
+                'B' => (byte) DisplayCharactersEnum.HexB,
+                'C' => (byte) DisplayCharactersEnum.HexC,
+                'D' => (byte) DisplayCharactersEnum.HexD,
+                'E' => (byte) DisplayCharactersEnum.HexE,
+                'F' => (byte) DisplayCharactersEnum.HexF,
+                'a' => (byte) DisplayCharactersEnum.HexA,
+                'b' => (byte) DisplayCharactersEnum.HexB,
+                'c' => (byte) DisplayCharactersEnum.HexC,
+                'd' => (byte) DisplayCharactersEnum.HexD,
+                'e' => (byte) DisplayCharactersEnum.HexE,
+                'f' => (byte) DisplayCharactersEnum.HexF,
                 _ => (byte) DisplayCharactersEnum.None
             };
         }
 
-        private void SI(byte ch)
+        private void ResetSr()
         {
-            Console.Write(@"SerialOutput[");
-            for (var i = 0; i < 8; i++)
-            {
-                var val = (ch & (0x80 >> i)) > 0;
-                Console.Write(i+@":"+Convert.ToInt16(val));
-                _sdiPin.PinWrite(val);
-                _indPin.PinWrite(val);
-                Thread.Sleep(20);
-                Pulse(_srclkPin);
-            }
-            Console.WriteLine(@"]");
+            _sr.ShiftClear();
+        }
+
+        private void Si(byte ch)
+        {
+            _sr.ShiftByte(ch);
         }
 
         #endregion Private Methods
 
         #region Public Methods
-
-        /// <summary>
-        /// Send a triple of either 0/1/0 to a pin or 1/0/1 (in case of <see cref="inverted">inverted</see>=true)
-        /// </summary>
-        /// <param name="pin">Some <see cref="PinModel">Pinmodel</see> instantiated GPIO pin</param>
-        /// <param name="inverted">This parameter allows us to interact with active_low pins, whose default behaviour is to be always enabled and thus need an off pulse to act</param>
-        /// <remarks>We're using timespan here to get near nano-second accuracy for pulsing (as close as we need it to be within 500 nanos)</remarks>
-        private static void Pulse(PinModel pin, bool inverted = false)
-        {
-            pin.PinWrite(inverted);
-            Thread.Sleep(TimeSpan.FromMilliseconds(0.5d));
-            pin.PinWrite(!inverted);
-            Thread.Sleep(TimeSpan.FromMilliseconds(0.5d));
-            pin.PinWrite(inverted);
-            Thread.Sleep(TimeSpan.FromMilliseconds(0.5d));
-        }
 
         public void RunTest()
         {
@@ -163,49 +108,49 @@ namespace RpiLED.Core.Services
             Console.WriteLine(@"Test1");
             for (var i = 0; i < 8; i++)
             {
-                SI(ShiftOutput[i]);
-                Pulse(_rclkPin);
+                Si(ShiftOutput[i]);
                 Thread.Sleep(200);
             }
+
             Console.WriteLine(@"Reset");
             Thread.Sleep(500);
             for (var i = 0; i < 4; i++)
             {
-                SI(0xff);
-                Pulse(_rclkPin);
+                Si(0xff);
                 Thread.Sleep(200);
-                SI(0x00);
+                Si(0x00);
                 Thread.Sleep(200);
             }
+
             Console.WriteLine(@"Test2");
             Thread.Sleep(500);
             for (var i = 0; i < 8; i++)
             {
-                SI(ShiftOutput[8 - i - 1]);
-                Pulse(_rclkPin);
+                Si(ShiftOutput[8 - i - 1]);
                 Thread.Sleep(200);
             }
+
             Console.WriteLine(@"Reset");
             Thread.Sleep(500);
             for (var i = 0; i < 4; i++)
             {
-                SI(0xff);
-                Pulse(_rclkPin);
+                Si(0xff);
                 Thread.Sleep(200);
-                SI(0x00);
+                Si(0x00);
                 Thread.Sleep(200);
             }
+
             ResetSr();
-            Console.WriteLine($@"testing HexA");
-            SI((byte)DisplayCharactersEnum.HexA);
+            Console.WriteLine(@"testing HexA");
+            Si((byte) DisplayCharactersEnum.HexA);
             Thread.Sleep(1000);
             ResetSr();
-            Console.WriteLine($@"testing Hex0");
-            SI((byte)DisplayCharactersEnum.HexZero);
+            Console.WriteLine(@"testing Hex0");
+            Si((byte) DisplayCharactersEnum.HexZero);
             Thread.Sleep(1000);
             ResetSr();
-            Console.WriteLine($@"testing Hex8");
-            SI((byte)DisplayCharactersEnum.HexEight);
+            Console.WriteLine(@"testing Hex8");
+            Si((byte) DisplayCharactersEnum.HexEight);
             Thread.Sleep(1000);
             ResetSr();
         }
@@ -213,25 +158,10 @@ namespace RpiLED.Core.Services
         public void ShiftIn(char c)
         {
             ResetSr();
-            var stringPattern = ToBinary(GetBitPattern(c));
-            Console.WriteLine(@"This is the byte sequence we're gonna send : " + stringPattern);
-            if (stringPattern.Length < 8)
-            {
-                throw new AccessViolationException($@"Not enough bits supplied (found:{stringPattern.Length} bits, require 8 bits)");
-            }
-            Console.Write(@"Writing : [");
-            for (var i = 0; i <= 7; i++)
-            {
-                //var val = pattern & Convert.ToInt16(0x80 >> i > 0);
-                var singleValue = stringPattern[i].ToString();
-                var val = Convert.ToBoolean(int.Parse(singleValue));
-                Console.Write(i + @":" + singleValue+@"|");
-                _sdiPin.PinWrite(val);
-                Pulse(_srclkPin);
-            }
-            Console.WriteLine(@"]");
+            var bitPattern = GetBitPattern(c);
+            Console.WriteLine(@"This is the byte sequence we're gonna send : " + bitPattern);
+            _sr.ShiftByte(bitPattern);
             Thread.Sleep(100);
-            Pulse(_rclkPin);
         }
 
         #endregion Public Methods
